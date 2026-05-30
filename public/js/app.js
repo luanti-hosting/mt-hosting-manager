bc11097ee3512efb5be55d8c117b00e7933f7f3c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2026-05-30 09:43:47 +0200

    add note about upstream availability and price changes

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,17 @@ export default {
 			<error-toast/>
 			<div class="container-fluid">
 				<br>
+				<div class="alert alert-danger" role="alert">
+					<p>
+						<b>Server availability:</b> Due to price changes and limited availability on the vps-hoster side
+						(<a target="_new" href="https://www.hetzner.com/pressroom/standardization-and-price-adjustment-of-our-server-products/">official post from Hetzner</a>)
+						we can't guarantee that new nodes can be created anymore.
+					</p>
+					<p>
+						Existing nodes are still working as expected though, if you are unhappy with the situation
+						feel free to ask for a refund in the <router-link to="/tickets">Service-ticket</router-link> page.
+					</p>
+				</div>
 				<router-view></router-view>
 			</div>
 		</div>
